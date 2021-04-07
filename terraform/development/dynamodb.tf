df5f72fcbd86532d75233a9489abe107f7d82a3c
humulla <humairaa.mulla@hackney.gov.uk>
2021-04-07 14:27:29 +0100

    Dynamodb has been set up

---
+++ terraform/development/dynamodb.tf
@@ -0,0 +1,51 @@
+resource "aws_dynamodb_table" "contactdetailsapi_dynamodb_table" {
+    name                  = "contact-details"
+    billing_mode          = "PROVISIONED"
+    read_capacity         = 10
+    write_capacity        = 10
+    hash_key              = "targetId"
+    range_key             = "id"
+
+
+    attribute {
+        name              = "id"
+        type              = "S"
+    }
+
+    tags = {
+        Name              = "contactDetails-api-${var.environment_name}"
+        Environment       = var.environment_name
+        terraform-managed = true
+        project_name      = var.project_name
+    }
+}
+
+resource "aws_iam_policy" "contactdetailsapi_dynamodb_table_policy" {
+    name                  = "lambda-dynamodb-contact-details-api"
+    description           = "A policy allowing read/write operations on contact details dynamoDB for the contact details API"
+    path                  = "/contact-details-api/"
+
+    policy                = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                        "dynamodb:BatchGet*",
+                        "dynamodb:BatchWrite*",
+                        "dynamodb:DeleteItem",
+                        "dynamodb:DescribeStream",
+                        "dynamodb:DescribeTable",
+                        "dynamodb:Get*",
+                        "dynamodb:PutItem",
+                        "dynamodb:Query",
+                        "dynamodb:Scan",
+                        "dynamodb:UpdateItem"
+                     ],
+            "Resource": "${aws_dynamodb_table.contactdetailsapi_dynamodb_table.arn}"
+        }
+    ]
+}
+EOF
+}
---
+++ terraform/production/dynamodb.tf
@@ -0,0 +1,47 @@
+resource "aws_dynamodb_table" "contactdetailsapi_dynamodb_table" {
+    name                  = "Contact Details"
+    billing_mode          = "PROVISIONED"
+    read_capacity         = 10
+    write_capacity        = 10
+    hash_key              = "targetId"
+    range_key             = "id"
+
+    attribute {
+        name              = "id"
+        type              = "S"
+    }
+
+    tags = {
+        Name              = "contact-details-api-${var.environment_name}"
+        Environment       = var.environment_name
+        terraform-managed = true
+        project_name      = var.project_name
+    }
+}
+
+resource "aws_iam_policy" "contactdetailsapi_dynamodb_table_policy" {
+    name                  = "lambda-dynamodb-contact-details-api"
+    description           = "A policy allowing read/write operations on contact details dynamoDB for the contact details API"
+    path                  = "/contact-details-api/"
+
+    policy                = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                        "dynamodb:BatchGetItem",
+                        "dynamodb:GetItem",
+                        "dynamodb:Query",
+                        "dynamodb:Scan",
+                        "dynamodb:BatchWriteItem",
+                        "dynamodb:PutItem",
+                        "dynamodb:UpdateItem"
+                     ],
+            "Resource": "${aws_dynamodb_table.contactdetailsapi_dynamodb_table.arn}"
+        }
+    ]
+}
+EOF
+}
