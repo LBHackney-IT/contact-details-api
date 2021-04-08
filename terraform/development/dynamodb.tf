32ddb446f932798c6130b8b178174ef5e0d3bd12
humulla <humairaa.mulla@hackney.gov.uk>
2021-04-08 08:49:50 +0100

    Modified table name

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "contactdetailsapi_dynamodb_table" {
-    name                  = "Contact Details"
+    name                  = "ContactDetails"
     billing_mode          = "PROVISIONED"
     read_capacity         = 10
     write_capacity        = 10
