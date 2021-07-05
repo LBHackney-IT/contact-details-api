4a89196a22db11012f4dc91a2663c57dfe8e2adc
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-05 12:24:40 +0100

    Trying to add SNS topic for testing purposes

--- ContactDetailsApi.Tests/AwsIntegrationTests.cs
+++ ContactDetailsApi.Tests/AwsIntegrationTests.cs
@@ -31,6 +31,8 @@ namespace ContactDetailsApi.Tests
 
         public AwsIntegrationTests()
         {
+            CreateSnsTopic();
+
             EnsureEnvVarConfigured("DynamoDb_LocalMode", "true");
             EnsureEnvVarConfigured("DynamoDb_LocalServiceUrl", "http://localhost:8000");
             EnsureEnvVarConfigured("Localstack_SnsServiceUrl", "http://localhost:4566");
@@ -65,17 +67,17 @@ namespace ContactDetailsApi.Tests
 
         private void CreateSnsTopic()
         {
-            //var snsAttrs = new Dictionary<string, string>();
-            //snsAttrs.Add("fifo_topic", "true");
-            //snsAttrs.Add("content_based_deduplication", "true");
+            var snsAttrs = new Dictionary<string, string>();
+            snsAttrs.Add("fifo_topic", "true");
+            snsAttrs.Add("content_based_deduplication", "true");
 
-            //var response = SimpleNotificationService.CreateTopicAsync(new CreateTopicRequest
-            //{
-            //    Name = "personcreated",
-            //    Attributes = snsAttrs
-            //}).Result;
+            var response = SimpleNotificationService.CreateTopicAsync(new CreateTopicRequest
+            {
+                Name = "personcreated",
+                Attributes = snsAttrs
+            }).Result;
 
-            //Environment.SetEnvironmentVariable("NEW_PERSON_SNS_ARN", response.TopicArn);
+            Environment.SetEnvironmentVariable("NEW_PERSON_SNS_ARN", response.TopicArn);
         }
     }
 
