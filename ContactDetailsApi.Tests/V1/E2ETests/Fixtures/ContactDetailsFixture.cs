8716eeb22a0564023f438088f0caa1b1bd15c651
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-05 13:21:51 +0100

    Trying to figure out what the error issue is.

--- ContactDetailsApi.Tests/V1/E2ETests/Fixtures/ContactDetailsFixture.cs
+++ ContactDetailsApi.Tests/V1/E2ETests/Fixtures/ContactDetailsFixture.cs
@@ -124,17 +124,17 @@ namespace ContactDetailsApi.Tests.V1.E2ETests.Fixtures
 
         private void CreateSnsTopic()
         {
-            var snsAttrs = new Dictionary<string, string>();
-            snsAttrs.Add("fifo_topic", "true");
-            snsAttrs.Add("content_based_deduplication", "true");
+            //var snsAttrs = new Dictionary<string, string>();
+            //snsAttrs.Add("fifo_topic", "true");
+            //snsAttrs.Add("content_based_deduplication", "true");
 
-            var response = _amazonSimpleNotificationService.CreateTopicAsync(new CreateTopicRequest
-            {
-                Name = "person",
-                Attributes = snsAttrs
-            }).Result;
+            //var response = _amazonSimpleNotificationService.CreateTopicAsync(new CreateTopicRequest
+            //{
+            //    Name = "person",
+            //    Attributes = snsAttrs
+            //}).Result;
 
-            Environment.SetEnvironmentVariable("PERSON_SNS_ARN", response.TopicArn);
+            //Environment.SetEnvironmentVariable("PERSON_SNS_ARN", response.TopicArn);
         }
     }
 }
