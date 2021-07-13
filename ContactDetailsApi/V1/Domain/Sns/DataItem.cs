899b5016bee6a6c3eac766f8bd143bead5208d4c
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-13 14:10:58 +0100

    Added new values

--- ContactDetailsApi/V1/Domain/Sns/DataItem.cs
+++ ContactDetailsApi/V1/Domain/Sns/DataItem.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace ContactDetailsApi.V1.Domain.Sns
 {
     public class DataItem
     {
+        public Guid Id { get; set; }
         public string Value { get; set; }
     }
 }
