35cca90e2f46a75e48b1953c52ee4f6229aaf617
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-08-09 15:23:36 +0100

    Added error code for XSS validation failure

--- ContactDetailsApi/V1/Boundary/Request/Validation/ErrorCodes.cs
+++ ContactDetailsApi/V1/Boundary/Request/Validation/ErrorCodes.cs
@@ -5,6 +5,6 @@ namespace ContactDetailsApi.V1.Boundary.Request.Validation
         // TODO - Use correct error codes...
         public const string InvalidEmail = "W40";
         public const string InvalidPhoneNumber = "W41";
-        public const string XssCheckFailure = "W666";
+        public const string XssCheckFailure = "W42";
     }
 }
