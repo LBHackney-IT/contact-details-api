0129b9bb4f2270e222f423faaf5c64f6b2e7e819
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-08-09 15:25:39 +0100

    Update ErrorCodes.cs

--- ContactDetailsApi/V1/Boundary/Request/Validation/ErrorCodes.cs
+++ ContactDetailsApi/V1/Boundary/Request/Validation/ErrorCodes.cs
@@ -2,7 +2,6 @@ namespace ContactDetailsApi.V1.Boundary.Request.Validation
 {
     public static class ErrorCodes
     {
-        // TODO - Use correct error codes...
         public const string InvalidEmail = "W40";
         public const string InvalidPhoneNumber = "W41";
         public const string XssCheckFailure = "W42";
