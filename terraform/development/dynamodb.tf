413d6d9e1caa75a6c15590bec8450a1892b7361b
Charnjit Sohal <charnjit.sohal@news.co.uk>
2021-09-14 14:05:05 +0100

    added CW dashboard

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -1,32 +1,29 @@
 resource "aws_dynamodb_table" "contactdetailsapi_dynamodb_table" {
-    name                  = "ContactDetails"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "targetId"
-    range_key             = "id"
+  name           = "ContactDetails"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "targetId"
+  range_key      = "id"
 
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-    
-    attribute {
-        name              = "targetId"
-        type              = "S"
-    }
 
-    tags = {
-        Name              = "contact-details-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        BackupPolicy     = "Dev"
-    }
+  attribute {
+    name = "targetId"
+    type = "S"
+  }
 
-    point_in_time_recovery {
-        enabled           = true
-    }
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Dev" }
+  )
+
+  point_in_time_recovery {
+    enabled = true
+  }
 }
--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -1,30 +1,27 @@
 resource "aws_dynamodb_table" "contactdetailsapi_dynamodb_table" {
-    name                  = "ContactDetails"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "targetId"
-    range_key             = "id"
+  name           = "ContactDetails"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "targetId"
+  range_key      = "id"
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-    attribute {
-        name              = "targetId"
-        type              = "S"
-    }
+  attribute {
+    name = "targetId"
+    type = "S"
+  }
 
-    tags = {
-        Name              = "contact-details-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        BackupPolicy     = "Stg"
-    }
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Stg" }
+  )
 
-    point_in_time_recovery {
-        enabled           = true
-    }
+  point_in_time_recovery {
+    enabled = true
+  }
 }
