4b07fb485fd4814c2e7c8ef718f4bcdbfbac67d6
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-02-07 10:21:44 +0000

    Change dynamodb to on-demand capacity

--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "contactdetailsapi_dynamodb_table" {
   name           = "ContactDetails"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 10
-  write_capacity = 10
+  billing_mode   = "PAY_PER_REQUEST"  
   hash_key       = "targetId"
   range_key      = "id"
 
