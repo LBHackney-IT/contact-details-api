d148a023ff77e7b96be567ac4e181eae4c60df89
Humairaa Mulla <humairaa.mulla@hackney.gov.uk>
2024-03-12 13:12:49 +0000

    nullable title

--- ContactDetailsApi/V2/Gateways/ContactDetailsDynamoDbGateway.cs
+++ ContactDetailsApi/V2/Gateways/ContactDetailsDynamoDbGateway.cs
@@ -244,7 +244,7 @@ namespace ContactDetailsApi.V2.Gateways
                     Id = (Guid) id,
                     FirstName = firstName,
                     Surname = surname,
-                    Title = (Title) Enum.Parse(typeof(Title), title ?? null)
+                    Title = (Title?) Enum.Parse(typeof(Title), title ?? null)
                 };
 
                 results.Add(entity);
