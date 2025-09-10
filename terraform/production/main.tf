ca8ca7954a7bcdcac4f75c3081b98e6e9497611e
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-09-10 09:54:44 +0100

    Comment out broken SSM parameter resources and CloudWatch alarm module in main.tf

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -53,11 +53,11 @@ resource "aws_sns_topic" "contactdetails_topic" {
   sqs_failure_feedback_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LBH_SNS_DELIVERY_LOGGING_ROLE"
 }
 
-resource "aws_ssm_parameter" "contact_details_sns_arn" {
-  name  = "/sns-topic/production/contact_details/arn"
-  type  = "String"
-  value = aws_sns_topic.contactdetails_topic.arn
-}
+# resource "aws_ssm_parameter" "contact_details_sns_arn" {
+#   name  = "/sns-topic/production/contact_details/arn"
+#   type  = "String"
+#   value = aws_sns_topic.contactdetails_topic.arn
+# }
 
 module "contact_details_api_cloudwatch_dashboard" {
   source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/dashboards/api-dashboard"
@@ -67,18 +67,18 @@ module "contact_details_api_cloudwatch_dashboard" {
   dynamodb_table_name = aws_dynamodb_table.contactdetailsapi_dynamodb_table.name
 }
 
-data "aws_ssm_parameter" "cloudwatch_topic_arn" {
-  name = "/housing-tl/${var.environment_name}/cloudwatch-alarms-topic-arn"
-}
+# data "aws_ssm_parameter" "cloudwatch_topic_arn" {
+#   name = "/housing-tl/${var.environment_name}/cloudwatch-alarms-topic-arn"
+# }
 
-module "api-alarm" {
-  source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/api-alarm"
-  environment_name = var.environment_name
-  api_name         = "contact-details-api"
-  alarm_period     = "300"
-  error_threshold  = "1"
-  sns_topic_arn    = data.aws_ssm_parameter.cloudwatch_topic_arn.value
-}
+# module "api-alarm" {
+#   source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/api-alarm"
+#   environment_name = var.environment_name
+#   api_name         = "contact-details-api"
+#   alarm_period     = "300"
+#   error_threshold  = "1"
+#   sns_topic_arn    = data.aws_ssm_parameter.cloudwatch_topic_arn.value
+# }
 
 # NOT WORKING NEEDS TO BE INVESTIGATED
 # module "sns-delivery-failure-alarm" {
